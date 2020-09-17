d169578023ac64f3a706a14e5465f4971056db15
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-17 01:08:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  new_array = [ ]
+  name_hash.collect do |individual_key, key_value|
+    new_array = key_value
+  end
+  min_key = nil
+  min_value = nil
+  if new_array[1] < new_array[2] && new_array[1] < new_array[3]
+    min_key = new_array[1]
+    return min_key[1]
+  end
+  
 end
