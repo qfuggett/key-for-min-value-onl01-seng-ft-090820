096961bda1f5e9e0ca01999a8a5973a5550f9544
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-17 01:11:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,10 +7,7 @@ def key_for_min_value(name_hash)
     new_array = key_value
     min_key = nil
     min_value = nil
-    if new_array[1] < new_array[2] && new_array[1] < new_array[3]
-      min_key = new_array[1]
-      return individual_key
-    end
+    
   end
   
   
