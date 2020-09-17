598a69aac86f4700d8de752d5e3b3e4947141028
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-17 01:37:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,9 +8,11 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < min_value || min_value == nil
       min_value = value
+      min_key = key
     end
     
   end
   
+  return min_key
   
 end
